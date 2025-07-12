538341a998486dd235366ae55139bfdd813532d7
Lior Azroel <lior.azroel@cyberbit.com>
2025-07-12 15:42:38 +0300

    test travis yml

--- app/app.py
+++ app/app.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=os.getenv("PORT"))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0",port=port)
